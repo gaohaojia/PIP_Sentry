94dba305ec5fa9c2d21d9ee262a818ba8d2b663c
gaohaojia <2723687563@qq.com>
2024-04-09 20:33:43 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt install -y ros-humble-gazebo-*
+sudo apt install -y ros-humble-xacro
+sudo apt install -y ros-humble-robot-state-publisher
+sudo apt install -y ros-humble-joint-state-publisher
+sudo apt install -y ros-humble-rviz2
+sudo apt install -y ros-humble-nav2*
+sudo apt install -y ros-humble-slam-toolbox
+sudo apt install -y ros-humble-pcl-ros
+sudo apt install -y ros-humble-pcl-conversions
+sudo apt install -y ros-humble-libpointmatcher
+sudo apt install -y ros-humble-tf2-geometry-msgs
+sudo apt install -y libboost-all-dev
+sudo apt install -y libgoogle-glog-dev
